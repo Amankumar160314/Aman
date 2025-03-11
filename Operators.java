1bebd0e54c7f32f49d11f4579109c080e071bc80
Amankumar160314 <amanrauniyar2004@gmail.com>
2025-03-11 10:39:05 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,12 @@
+public class Operators
+{
+public static void main(string[]args)
+{
+int x=10;
+system.out.println(x++);
+system.out.println(++x);
+system.out.println(x--);
+system.out.println(--x);
+} 
+}
+
